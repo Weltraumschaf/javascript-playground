9f765f8df6e5f1532dbc51da0fd913bdcc0b5ec6
Sven Strittmatter <ich@weltraumschaf.de>
2019-09-28 23:15:44 +0200

    Init scripts
    Signed-off-by: Sven Strittmatter <ich@weltraumschaf.de>

---
+++ README.md
@@ -0,0 +1,9 @@
+# JavaScript Playground
+
+Here I play around with JavaScript.
+
+## Create a new playground
+
+```bash
+generate-new-palyground.sh playground
+```
---
+++ bin/generate-new-palyground.sh
@@ -0,0 +1,60 @@
+#!/usr/bin/env bash
+
+set -eu
+
+playgoundName="${1:-}"
+usage="${0} <playgroundName>"
+
+if [ "${playgoundName}" = "" ]; then
+    echo "No playground name given!"
+    echo "${usage}"
+    exit 1
+fi
+
+if [ "${PROJECT_DIR}" = "" ]; then
+    echo "No PROJECT_DIR var exported!"
+    echo "Source the .envrc file."
+    exit 1
+fi
+
+playgroundDir="${PROJECT_DIR}/${playgoundName}"
+if [ -d "${playgroundDir}" ]; then
+    echo "There is already a playground in ${playgroundDir}!"
+    exit 2;
+fi
+
+mkdir -p "${playgroundDir}"
+
+cat << EOF > "${playgroundDir}/README.md"
+# ${playgoundName}
+EOF
+
+cat << EOF > "${playgroundDir}/main.css"
+#canvas {
+    background: #fefefe;
+    border: thin solid lightgray;
+    -webkit-box-shadow: 4px 4px 8px rgba(0, 0, 0, 0.5);
+    -moz-box-shadow: 4px 4px 8px rgba(0, 0, 0, 0.5);
+    box-shadow: 4px 4px 8px rgba(0, 0, 0, 0.5);
+}
+EOF
+
+cat << EOF > "${playgroundDir}/main.js"
+(function(global) {
+
+}(window));
+EOF
+
+cat << EOF > "${playgroundDir}/index.html"
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>${playgoundName}</title>
+         <link rel="stylesheet" type="text/css" href="main.css"/>
+    </head>
+    <body>
+        <canvas id="canvas"></canvas>
+        <script src="main.js"></script>
+    </body>
+</html>
+EOF
